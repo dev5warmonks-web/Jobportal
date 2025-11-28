e797cc56ece6bb4b6f02947ab1a324c947e0850c
dev4warmonks-tech <dev4.warmonks@gmail.com>
2025-11-28 13:09:39 +0530

    api change

--- frontend/app/api.js
+++ frontend/app/api.js
@@ -1,6 +1,6 @@
 "use client";
 
-const API_URL = "http://localhost:5000/api/jobs";
+const API_URL = "https://api.mindssparsh.com/api/jobs";
 
 export const getJobs = async () => await fetch(API_URL).then(res => res.json());
 export const getJob = async (id) => await fetch(`${API_URL}/${id}`).then(res => res.json());
