d0c38e911be2e26e88cb40a068ad9c15b54b430e
dev5warmonks-web <dev5.warmonks@gmail.com>
2025-12-10 14:24:21 +0530

    env and apiconfig file changed

--- frontend/app/config/apiConfig.js
+++ frontend/app/config/apiConfig.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://91.108.110.47:3002";   //server
-export const BASE_URL = "http://localhost:3002";
+export const BASE_URL = "http://91.108.110.47:3002";   //server
+// export const BASE_URL = "http://localhost:3002";
